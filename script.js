93d78588cf3899b26e99dc916aac4c1c853d1596
Amanoo8 <asehrawat297@gmail.com>
2026-06-24 16:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,91 @@
-function submitQuiz(){
+document.addEventListener(
+"DOMContentLoaded",
+function(){
 
-    let score = 0;
+let name =
+localStorage.getItem(
+"studentName"
+);
 
-    let q1 =
-    document.querySelector(
-        'input[name="q1"]:checked'
-    );
-
-    let q2 =
-    document.querySelector(
-        'input[name="q2"]:checked'
-    );
-
-    let q3 =
-    document.querySelector(
-        'input[name="q3"]:checked'
-    );
-
-    if(q1 && q1.value==="Queue"){
-        score++;
-    }
-
-    if(q2 &&
-       q2.value==="Programming Language"){
-        score++;
-    }
-
-    if(q3 &&
-       q3.value==="100"){
-        score++;
-    }
-
-    let percentage =
-    (score/3)*100;
-
-    document.getElementById("result")
-    .innerHTML =
-    "Your Score : "
-    + score +
-    "/3 (" +
-    percentage +
-    "%)";
-
-    localStorage.setItem(
-        "lastScore",
-        percentage
-    );
+if(document.getElementById(
+"welcome"
+)){
+document.getElementById(
+"welcome"
+).innerHTML =
+"Welcome " + name;
 }
 
-document
-.getElementById("themeBtn")
-.addEventListener("click",function(){
-
-    document.body.classList.toggle("dark");
-
 });
+
+let timeLeft = 60;
+
+let timer =
+setInterval(function(){
+
+let timerElement =
+document.getElementById(
+"timer"
+);
+
+if(timerElement){
+
+timerElement.innerHTML =
+"Time Left : " +
+timeLeft;
+
+timeLeft--;
+
+if(timeLeft < 0){
+
+clearInterval(timer);
+
+submitQuiz();
+
+}
+
+}
+
+},1000);
+
+function submitQuiz(){
+
+let score = 0;
+
+let q1 =
+document.querySelector(
+'input[name="q1"]:checked'
+);
+
+let q2 =
+document.querySelector(
+'input[name="q2"]:checked'
+);
+
+let q3 =
+document.querySelector(
+'input[name="q3"]:checked'
+);
+
+if(q1 &&
+q1.value=="Queue")
+score++;
+
+if(q2 &&
+q2.value==
+"Programming Language")
+score++;
+
+if(q3 &&
+q3.value=="100")
+score++;
+
+localStorage.setItem(
+"score",
+score + "/3"
+);
+
+window.location =
+"result.html";
+
+}
